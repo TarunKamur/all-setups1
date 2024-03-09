2955c4b18d414397683345d5241618afe1a7b1fa
B Tarun Kumar <152691126+TarunKamur@users.noreply.github.com>
2024-03-10 00:25:32 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -16,19 +16,17 @@ systemctl status jenkins.service
 
 
 ##Jenkins script using ubuntu##
-#!/bin/bash
-sudo apt update -y
-#sudo apt upgrade -y
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
-sudo apt update -y
-sudo apt install temurin-17-jdk -y
-/usr/bin/java --version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-                  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo apt install openjdk-11-jdk
+java --version
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-                  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-                              /etc/apt/sources.list.d/jenkins.list > /dev/null
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  
+sudo apt-get update
+sudo apt-get install jenkins
 sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
